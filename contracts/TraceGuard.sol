c660cf834d2c7b94a9eb901eeb11e0b34245dd3b
Elavarasi <elanelavarasi06@gmail.com>
2026-07-31 01:23:46 +0530

    Connect React frontend with Hardhat and implement add/search batch

--- contracts/TraceGuard.sol
+++ contracts/TraceGuard.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 contract TraceGuard {
-
     struct Batch {
         string batchId;
         string productName;
@@ -14,109 +13,39 @@ contract TraceGuard {
         string recallReason;
     }
 
-    // Stores all batches using Batch ID
     mapping(string => Batch) public batches;
 
-    // Add a new product batch
     function addBatch(
         string memory _batchId,
         string memory _productName,
         string memory _mfgDate,
         string memory _expiryDate
     ) public {
-
-        require(
-            bytes(batches[_batchId].batchId).length == 0,
-            "Batch already exists"
+        batches[_batchId] = Batch(
+            _batchId, _productName, msg.sender,
+            _mfgDate, _expiryDate, msg.sender, false, ""
         );
-
-        batches[_batchId] = Batch({
-            batchId: _batchId,
-            productName: _productName,
-            manufacturer: msg.sender,
-            mfgDate: _mfgDate,
-            expiryDate: _expiryDate,
-            currentOwner: msg.sender,
-            isRecalled: false,
-            recallReason: ""
-        });
     }
 
-    // Transfer ownership of a batch
-    function transferOwnership(
-        string memory _batchId,
-        address _newOwner
-    ) public {
-
-        require(
-            bytes(batches[_batchId].batchId).length != 0,
-            "Batch does not exist"
-        );
-
-        require(
-            msg.sender == batches[_batchId].currentOwner,
-            "Only current owner can transfer ownership"
-        );
-
+    function transferOwnership(string memory _batchId, address _newOwner) public {
+        require(msg.sender == batches[_batchId].currentOwner, "Not the owner");
         batches[_batchId].currentOwner = _newOwner;
     }
 
-    // Recall a batch
-    function recallBatch(
-        string memory _batchId,
-        string memory _reason
-    ) public {
-
-        require(
-            bytes(batches[_batchId].batchId).length != 0,
-            "Batch does not exist"
-        );
-
-        require(
-            msg.sender == batches[_batchId].manufacturer,
-            "Only manufacturer can recall"
-        );
-
+    function recallBatch(string memory _batchId, string memory _reason) public {
+        require(msg.sender == batches[_batchId].manufacturer, "Not the manufacturer");
         batches[_batchId].isRecalled = true;
         batches[_batchId].recallReason = _reason;
     }
 
-    // Get complete batch details
-    function getBatchDetails(
-        string memory _batchId
-    )
-        public
-        view
-        returns (
-            string memory,
-            string memory,
-            address,
-            string memory,
-            string memory,
-            address,
-            bool,
-            string memory
-        )
-    {
-        Batch memory b = batches[_batchId];
-
-        return (
-            b.batchId,
-            b.productName,
-            b.manufacturer,
-            b.mfgDate,
-            b.expiryDate,
-            b.currentOwner,
-            b.isRecalled,
-            b.recallReason
-        );
+    function getBatchDetails(string memory _batchId) public view returns (Batch memory) {
+        return batches[_batchId];
     }
 
-    // Check if a batch is recalled
-    function checkStatus(
-        string memory _batchId
-    ) public view returns (bool) {
-
-        return batches[_batchId].isRecalled;
+    function checkStatus(string memory _batchId) public view returns (string memory) {
+        if (batches[_batchId].isRecalled) {
+            return "RECALLED";
+        }
+        return "SAFE";
     }
 }
